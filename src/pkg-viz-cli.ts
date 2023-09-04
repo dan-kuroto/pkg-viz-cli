a8d5bb385a57c3b03ecd45f77c12879ae8e9c837
dan-kuroto <2261268657@qq.com>
2023-09-04 21:33:14 +0800

    test(*): 加入echarts官方示例数据用于测试
    目前问题：
    - open库编译后导不进来
    - echarts需要的DOM对象在node.js里不支持

--- src/pkg-viz-cli.ts
+++ src/pkg-viz-cli.ts
@@ -4,6 +4,7 @@ import * as fs from "fs";
 import * as echarts from "echarts";
 import * as os from "os";
 import * as path from "path";
+import open from "open";
 import { pkgAnalyze } from './utils';
 
 
@@ -23,23 +24,117 @@ const options = program.version('1.0.0')
     .opts();
 
 const depth = toInt(options.depth, Infinity);
-const filePath = options.json ? String(options.json) : '';
+const jsonPath = options.json ? String(options.json) : '';
 const indent = toInt(options.indent, 2);
 
 const [dependency,] = pkgAnalyze(process.cwd(), depth);
-if (filePath) {
-    fs.writeFileSync(filePath, JSON.stringify(
+if (jsonPath) {
+    fs.writeFileSync(jsonPath, JSON.stringify(
         dependency,
         (key: string, value: any) => key === 'depth' ? undefined : value,
         indent
     ));
 } else {
+    const chartName = new Date().getTime();
+    const chartPath = path.join(os.tmpdir(), 'pkg-viz', `${chartName}.html`);
     const chart = echarts.init(document.createElement('div'));
-    chart.setOption({});
-    
-    // TODO: 还是不要用tmpdir了，在自己的目录下创建一个tmp，然后每次运行时删除超过一天的
-    // fs.writeFileSync(
-    //     path.join(os.tmpdir(), `${new Date().getTime}.html`),
-    //     chart.getDom().outerHTML
-    // );
+    // 暂时用echarts给的示例，先测出能不能正常生成HTML=>保存=>打开浏览器
+    chart.setOption({
+        title: {
+            text: 'Basic Graph'
+        },
+        tooltip: {},
+        animationDurationUpdate: 1500,
+        animationEasingUpdate: 'quinticInOut',
+        series: [
+            {
+                type: 'graph',
+                layout: 'none',
+                symbolSize: 50,
+                roam: true,
+                label: {
+                    show: true
+                },
+                edgeSymbol: ['circle', 'arrow'],
+                edgeSymbolSize: [4, 10],
+                edgeLabel: {
+                    fontSize: 20
+                },
+                data: [
+                    {
+                        name: 'Node 1',
+                        x: 300,
+                        y: 300
+                    },
+                    {
+                        name: 'Node 2',
+                        x: 800,
+                        y: 300
+                    },
+                    {
+                        name: 'Node 3',
+                        x: 550,
+                        y: 100
+                    },
+                    {
+                        name: 'Node 4',
+                        x: 550,
+                        y: 500
+                    }
+                ],
+                // links: [],
+                links: [
+                    {
+                        source: 0,
+                        target: 1,
+                        symbolSize: [5, 20],
+                        label: {
+                            show: true
+                        },
+                        lineStyle: {
+                            width: 5,
+                            curveness: 0.2
+                        }
+                    },
+                    {
+                        source: 'Node 2',
+                        target: 'Node 1',
+                        label: {
+                            show: true
+                        },
+                        lineStyle: {
+                            curveness: 0.2
+                        }
+                    },
+                    {
+                        source: 'Node 1',
+                        target: 'Node 3'
+                    },
+                    {
+                        source: 'Node 2',
+                        target: 'Node 3'
+                    },
+                    {
+                        source: 'Node 2',
+                        target: 'Node 4'
+                    },
+                    {
+                        source: 'Node 1',
+                        target: 'Node 4'
+                    }
+                ],
+                lineStyle: {
+                    opacity: 0.9,
+                    width: 2,
+                    curveness: 0
+                }
+            }
+        ]
+    });
+    fs.writeFileSync(
+        chartPath,
+        chart.getDom().outerHTML
+    );
+    console.log(chartPath);
+    open(`file://${chartPath}`);
 }
